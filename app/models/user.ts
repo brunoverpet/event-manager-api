2a6800fb7cbba889a5e42f29e6e553e3a46f9857
brunoverpet <bverpet46@gmail.com>
2025-06-19 10:54:06 +0200

    configure model+migration (users)

--- app/models/user.ts
+++ app/models/user.ts
@@ -14,13 +14,19 @@ export default class User extends compose(BaseModel, AuthFinder) {
   declare id: number
 
   @column()
-  declare fullName: string | null
+  declare lastname: string
+
+  @column()
+  declare firstname: string
 
   @column()
   declare email: string
 
   @column({ serializeAs: null })
   declare password: string
+
+  @column()
+  declare $isDeleted: boolean
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
